d4da275016d2978be5e6cdc6bddfc2d47f2bcdf1
ryuryuKG <ito.ryuta.11029@gmail.com>
2023-06-14 15:37:16 +0900

    new

--- Rja_1.py
+++ Rja_1.py
@@ -1,11 +1,9 @@
 import re
-
-sum=0
 with open('data.txt') as f:
-    lines = f.read()
-    for l in lines.split("\n"):
-        if re.fullmatch(r"-?\d+",l):
-            sum=sum+int(l)
-#    sum = sum+(int(l) for l in f.read().split("\n")if re.fullmatch(r"-?\d+",l))
+#     lines = f.read()
+#     for l in lines.split("\n"):
+#         if re.fullmatch(r"-?\d+",l):
+#             sum=sum+int(l)
+    sum=sum([int(l) for l in f.read().split("\n") if re.fullmatch(r"-?\d+",l)])
 print(sum)
 
