76c1adb5e15b4a31591045081a1e9d57e59a6bfd
ryuryuKG <ito.ryuta.11029@gmail.com>
2023-06-21 16:56:29 +0900

    Rj3

--- Rja_3.py
+++ Rja_3.py
@@ -1,6 +1,6 @@
 import glob
 l=[]
-for txt in glob.glob('sample/kitamura_****[1,3,5,7,9]_kgu.txt'):
+for txt in glob.glob('sample/kitamura_*[1,3,5,7,9]_kgu.txt'):
     with open(txt) as f:
         l.append(int(f.read()))
 print(sum(l))
